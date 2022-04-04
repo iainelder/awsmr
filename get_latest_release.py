de9072899e3eb43116fb9412c68b04e19d6e9d45
Iain Samuel McLean Elder <iain@isme.es>
2022-04-04 13:44:17 +0200

    Add main entrypoint for get_latest_release

--- get_latest_release.py
+++ get_latest_release.py
@@ -2,6 +2,10 @@ import requests
 from more_itertools import tail
 
 
+def main() -> None:
+    print(get_latest_release())
+
+
 def get_latest_release() -> str:
     """Returns the latest release of the AWS CLI version 1.x branch."""
     tags = requests.get("https://api.github.com/repos/aws/aws-cli/git/refs/tags").json()
@@ -12,3 +16,7 @@ def get_latest_release() -> str:
         return last_tag["ref"].rsplit("/", maxsplit=1)[1]
     except StopIteration:
         raise AssertionError(tags)
+
+
+if __name__ == "__main__":
+    main()
