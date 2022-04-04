64275af5f1baef43ecd92a0e0102097be307766b
Iain Samuel McLean Elder <iain@isme.es>
2022-04-05 00:30:45 +0200

    Improve test class name

--- tests/test_get_latest_release.py
+++ tests/test_get_latest_release.py
@@ -14,11 +14,11 @@ class Ref(TypedDict):
     ref: str
 
 
-RefFactory = Callable[[List[Ref]], None]
+RefRegistrar = Callable[[List[Ref]], None]
 
 
 @pytest.fixture()
-def register_refs() -> Generator[RefFactory, None, None]:
+def register_refs() -> Generator[RefRegistrar, None, None]:
 
     httpretty.enable(allow_net_connect=False, verbose=True)
 
@@ -44,18 +44,18 @@ def tmp_cwd(tmp_path: Path):
     chdir(old_cwd)
 
 
-def test_error_when_no_1_version(register_refs: RefFactory) -> None:
+def test_error_when_no_1_version(register_refs: RefRegistrar) -> None:
     register_refs([Ref(ref="refs/tags/2.0")])
     with pytest.raises(AssertionError):
         get_latest_release()
 
 
-def test_get_one_release(register_refs: RefFactory) -> None:
+def test_get_one_release(register_refs: RefRegistrar) -> None:
     register_refs([Ref(ref="refs/tags/1.0")])
     assert get_latest_release() == "1.0"
 
 
-def test_get_later_release(register_refs: RefFactory) -> None:
+def test_get_later_release(register_refs: RefRegistrar) -> None:
     register_refs([Ref(ref="refs/tags/1.0"), Ref(ref="refs/tags/1.1")])
     assert get_latest_release() == "1.1"
 
@@ -65,7 +65,7 @@ def test_write_latest_release(tmp_cwd: Path):
     assert (tmp_cwd / "aws_cli_release").read_text() == "1.23.45"
 
 
-def test_main(register_refs: RefFactory, tmp_cwd: Path):
+def test_main(register_refs: RefRegistrar, tmp_cwd: Path):
     register_refs([Ref(ref="refs/tags/1.0"), Ref(ref="refs/tags/1.1")])
     main()
     assert (tmp_cwd / "aws_cli_release").read_text() == "1.1"
